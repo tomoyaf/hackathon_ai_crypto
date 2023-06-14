1a39f396a7e307866906babfb98c7c96636839fe
poteboy <poteboy.dev@gmail.com>
2023-06-15 01:02:32 +0900

    list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useFeedItems } from "@/hooks/useFeedItems";
 import React from "react";
 import { signIn, useSession } from "next-auth/react";
 import { k } from "@kuma-ui/core";
-import { Header, BottomNav, Layout } from "@/components";
+import { Header, BottomNav, Layout, ListItem } from "@/components";
 
 export default function IndexPage() {
   const items = useFeedItems();
@@ -15,6 +15,11 @@ export default function IndexPage() {
     >
       <p>サンプル曲をリスト表示して再生できる</p>
       <p>声リンクをクリックで声詳細ページに遷移</p>
+      <k.div mt="100px"></k.div>
+
+      {[...Array(50)].map((k, i) => {
+        return <ListItem key={i} />;
+      })}
     </k.div>
   );
 }
