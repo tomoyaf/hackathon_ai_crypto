9e9c3ebb9c8a48b09c9d0ad8c3a0929e9b0f165e
poteboy <poteboy.dev@gmail.com>
2023-06-18 19:00:55 +0900

    voiceModel呼ぶ

--- src/pages/voices/[id]/index.tsx
+++ src/pages/voices/[id]/index.tsx
@@ -2,9 +2,21 @@ import { Layout, SongItem } from "@/components";
 import { k } from "@kuma-ui/core";
 import { useFeedItems } from "@/hooks/useFeedItems";
 import React from "react";
+import { InferGetServerSidePropsType } from "next";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { VoiceModel } from "@prisma/client";
 
-export default function IndexPage() {
+type ServerProps = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+export default function IndexPage({ initialData }: ServerProps) {
   const items = useFeedItems();
+  const router = useRouter();
+  const { id } = router.query;
+  const { data = initialData, isLoading } = useSWR<VoiceModel>(
+    `/api/voiceModels/${id}`,
+    (url: string) => fetch(url).then((res) => res.json())
+  );
 
   return (
     <k.div
@@ -15,3 +27,12 @@ export default function IndexPage() {
 }
 
 IndexPage.getLayout = (page: any) => <Layout>{page}</Layout>;
+
+export async function getServerSideProps({ params }: any) {
+  const res = await fetch(`http://localhost:3009/api/voiceModels/${params.id}`);
+  const initialData: VoiceModel = await res.json();
+
+  return {
+    props: { initialData },
+  };
+}
