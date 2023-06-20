42ce3d5aa8d1106b52fa948c107a6b7bebf92939
tomoyaf <gazimum@gmail.com>
2023-06-20 16:10:36 +0900

    private to env

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,9 +1,21 @@
 import { Storage, AddAclOptions, SaveOptions } from "@google-cloud/storage";
-import nodePath from "node:path";
 
 export const storage = new Storage({
   projectId: "aicrypto-389808",
-  keyFilename: nodePath.resolve("./key/gcloud.json"),
+  credentials: {
+    type: "service_account",
+    project_id: "aicrypto-389808",
+    private_key_id: process.env.GCLOUD_PRIVATE_KEY_ID ?? "",
+    private_key: process.env.GCLOUD_PRIVATE ?? "",
+    client_email: "api-server@aicrypto-389808.iam.gserviceaccount.com",
+    client_id: "114231271332667009312",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url:
+      "https://www.googleapis.com/robot/v1/metadata/x509/api-server%40aicrypto-389808.iam.gserviceaccount.com",
+    universe_domain: "googleapis.com",
+  } as any,
 });
 
 export async function createBucket(
